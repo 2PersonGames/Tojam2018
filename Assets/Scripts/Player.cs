8596a815fbb31899173ea5a9ea5b25eb585558b1
Sam Albon <sam.m.albon@outlook.com>
2018-05-05 19:58:03 -0400

    Added audio source and audio clip

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,16 @@ public class Player : MonoBehaviour
     
     [Range(1, 100)]
     public int Happiness;
+    public AudioClip BlobCreatedAudioClip;
+    public AudioClip BlobAbsorbedAudioClip;
+    public AudioClip PlayerHitWall;
 
+    private AudioSource _audioSource;
     private int playerNumber_;
 
     public void Init()
     {
+        _audioSource = GetComponent<AudioSource>();
         Happiness = 50;
         playerNumber_ = ++playerCounter;
     }
@@ -27,15 +32,32 @@ public class Player : MonoBehaviour
 
     }
 
+    void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (collision2D.gameObject.name == "Blocks_1")
+        {
+            Debug.Log(string.Format("Player collided with {0}!", gameObject.name));
+            _audioSource.PlayOneShot(PlayerHitWall, 1.0f);
+        }
+    }
+
     public void BlobCreated(GameObject blog)
     {
         Happiness--;
+        if (false)
+        {
+            _audioSource.PlayOneShot(BlobCreatedAudioClip, 1.0f);
+        }
         Debug.Log(string.Format("Player happiness decreased to {0}", Happiness));
     }
 
     public void ConsumeBlob(BlobController blobController)
     {
         Happiness++;
+        if (false)
+        {
+            _audioSource.PlayOneShot(BlobAbsorbedAudioClip, 1.0f);
+        }
         Debug.Log(string.Format("Player happiness increased to {0}", Happiness));
     }
 }
