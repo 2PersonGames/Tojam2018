ef9455b23d3e508225c963f21903c7e558a32a55
Ian Nastajus <ian.nastajus@gmail.com>
2018-05-05 02:58:11 -0400

    started making player

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        var foundPlayers = FindObjectsOfType<Player>();
+        foreach (var player in foundPlayers)
+        {
+            player.Init();
+
+            //maybe assign controls here
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private static int playerCounter = 0; 
+    private int playerNumber_;
+
+    public void Init()
+    {
+        playerNumber_ = ++playerCounter;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
