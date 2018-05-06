2c0523f6f93b5a590fe194b8a8d4656f261b3c01
Sam Albon <sam.m.albon@outlook.com>
2018-05-06 15:29:54 -0400

    Added beginning of death particle effect

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,6 +19,7 @@ public class Player : MonoBehaviour
 
     [Range(1, 100)]
     public int Happiness;
+    public GameObject DeathParticlePrefab;
     public AudioClip BlobCreatedAudioClip;
     public AudioClip BlobAbsorbedAudioClip;
     public AudioClip PlayerHitWall;
@@ -71,7 +72,8 @@ public class Player : MonoBehaviour
             {
                 Debug.Log(string.Format("Player has sacrified themselves for another player!"));
                 otherPlayer.ConsumeHappiness(this);
-                Destroy(gameObject);                
+                Instantiate(DeathParticlePrefab, gameObject.transform.position, Quaternion.identity);
+                Destroy(gameObject);               
                 Debug.Log("TODO: Handle win state");
             }
         }
